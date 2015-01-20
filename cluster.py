de51088f55dd04cd6b9d6aa7b327d703a1731f54
Matias Lespiau <mlespiau@avature.net>
2015-01-20 17:15:30 -0300

    Move Segmenter class to cluster.py

--- cluster.py
+++ cluster.py
@@ -1,4 +1,5 @@
 import numpy
+import scipy.stats.mstats as stats
 
 class Cluster():
     def __init__(self, name, gmm, initialSegment):
@@ -62,3 +63,44 @@ class Segment():
 
     def getEnd(self):
         return self.end
+
+class Resegmenter():
+    def __init__(self, X, N, cluster_list):
+        self.X = X
+        self.N = N
+        self.cluster_list = cluster_list
+        self.number_of_clusters = len(self.cluster_list)
+        self.interval_size = 150
+
+    def execute(self):
+        likelihoods = self.cluster_list[0].get_gmm().score(self.X)
+        self.cluster_list[0].reset_data()
+        for cluster in self.cluster_list[1:]:
+            likelihoods = numpy.column_stack((likelihoods, cluster.get_gmm().score(self.X)))
+            cluster.reset_data()
+        if self.number_of_clusters == 1:
+            self.most_likely = numpy.zeros(len(self.X))
+        else:
+            self.most_likely = likelihoods.argmax(axis=1)
+        # Across 250 frames of observations
+        # Vote on wich cluster they should be associated with
+        data_range = range(0, self.N, self.interval_size)
+        if data_range[-1] < self.N:
+            data_range.append(self.N)
+        for i, v in enumerate(data_range[0:len(data_range)-1]):
+            current_segment_indexes = range(data_range[i], data_range[i+1])
+            current_segment_scores = numpy.array(self.most_likely[current_segment_indexes])
+            # print(current_segment_data)
+            most_likely_gmm_class = int(stats.mode(current_segment_scores)[0][0])
+            print(most_likely_gmm_class)
+            # print(self.X[current_segment_indexes,:])
+            current_segment_data = self.X[current_segment_indexes,:]
+            segment = Segment(data_range[i], data_range[i+1], current_segment_data)
+            segment.set_most_likely_gmm_class(self.cluster_list[most_likely_gmm_class].get_name())
+            self.cluster_list[most_likely_gmm_class].add_segment(segment)
+        new_cluster_list = []
+        for cluster in self.cluster_list:
+            if len(cluster.get_segments()) > 0:
+                cluster.train_gmm()
+                new_cluster_list.append(cluster)
+        return new_cluster_list
